aceae9abacb3bfc55efe6936d285409f58e15ba9
lishuai2016 <1830473670@qq.com>
2018-02-16 10:19:25 +0800

    Initial commit

---
+++ eureka-feign/src/main/java/com/didispace/service/ComputeClient.java
@@ -0,0 +1,15 @@
+package com.didispace.service;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+//添加熔断机制只需要添加这一个ComputeClientHystrix即可
+//@FeignClient("compute-service")
+@FeignClient(value = "compute-service", fallback = ComputeClientHystrix.class)
+public interface ComputeClient {
+
+    @RequestMapping(method = RequestMethod.GET, value = "/add")
+    Integer add(@RequestParam(value = "a") Integer a, @RequestParam(value = "b") Integer b);
+
+}
---
+++ eureka-ribbon/src/main/java/com/didispace/web/ConsumerController.java
@@ -0,0 +1,28 @@
+package com.didispace.web;
+
+import com.didispace.service.ComputeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class ConsumerController {
+//非熔断模式
+//    @Autowired
+//    RestTemplate restTemplate;
+//
+//    @RequestMapping(value = "/add", method = RequestMethod.GET)
+//    public String add() {
+//        return restTemplate.getForEntity("http://COMPUTE-SERVICE/add?a=10&b=20", String.class).getBody();
+//    }
+
+    @Autowired
+    private ComputeService computeService;
+
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public String add() {
+        return computeService.addService();
+    }
+}
